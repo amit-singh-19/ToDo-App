693e988b27c84b42d2c92ec100be2f05157d76b3
amit-singh-19 <vinitsingh4477@gmail.com>
2025-07-30 13:54:53 +0530

    initialized frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="bg-slate-900 min-h-screen flex items-center justify-center text-white">
+      <h1 className='text-4xl font-bold'>MERN Todo App</h1>
+    </div>
+  )
+}
