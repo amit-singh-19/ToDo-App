d40a7d96ebd3954caf79b42abf9f4879e38d8390
amit-singh-19 <vinitsingh4477@gmail.com>
2025-08-01 15:22:03 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import RegisterPage from "../src/pages/RegisterPage";
 
 export default function App() {
   return (
-    <div className="bg-slate-900 min-h-screen flex items-center justify-center text-white">
-      <h1 className='text-4xl font-bold'>MERN Todo App</h1>
+    <div>
+      <RegisterPage />
     </div>
-  )
+  );
 }
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function RegisterPage() {
   return (
-    <div>RegisterPage</div>
+    <div className='min-h-screen bg-slate-900 text-white flex flex-col items-center justify-center'>
+      <div className='w-full max-w-md p-8 space-y-8 bg-slate-800 rounded-xl '>
+        <div className="text-center">
+
+        </div>
+      </div>
+    </div>
   )
 }
